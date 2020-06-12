7f406d87bca1115ad5c20843c547fe48966b3a72
riteshprk <reach2ritesh@gmail.com>
2020-06-12 20:26:00 +0100

    Added port 8080 to run app

--- app.py
+++ app.py
@@ -233,6 +233,7 @@ def create_app():
     return app
 
 
+app = create_app()
+
 if __name__ == "__main__":
-    app = create_app()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
