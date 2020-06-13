7597df16fc0fc16d1bbc95b5f3c8083bec36575e
riteshprk <reach2ritesh@gmail.com>
2020-06-13 12:31:43 +0100

    updated test database

--- test_app.py
+++ test_app.py
@@ -24,8 +24,8 @@ class CapstoneTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "capstone_test"
-        self.database_path = "postgresql://{}@{}/{}".format(
-            'postgres:temp101', 'localhost:5432', self.database_name)
+        self.database_path = "postgres://qqnqskavunpmlp:3c061430cd6fecdbca498694d6aad4ec05f91ab95b8ecb96a23f6623cfa9afd3@ec2-54-247-78-30.eu-west-1.compute.amazonaws.com:5432/de5ihjb8ts6v0j"
+        #"postgresql://{}@{}/{}".format('postgres:temp101', 'localhost:5432', self.database_name)
 
         # binds the app to the current context
         with self.app.app_context():
@@ -151,121 +151,6 @@ class CapstoneTestCase(unittest.TestCase):
             "code": "unauthorized",
             "description": "Permission not found."
         })
-
-    # def test_casting_director_success(self):
-    #     headers = {'Content-Type': 'application/json',
-    #                'Authorization': 'Bearer {}'.format(casting_director_token)}
-    #     res = self.client().delete('/actors/1', headers=headers)
-    #     data = json.loads(res.data)
-    #     if res.status_code == 200:
-    #         self.assertEqual(res.status_code, 200)
-    #         self.assertEqual(data['success'], True)
-    #     else:
-    #         self.assertEqual(res.status_code, 422)
-    #         self.assertEqual(data['success'], False)
-
-    # def test_casting_assitant_permission_fail(self):
-    #     headers = {'Content-Type': 'application/json',
-    #                'Authorization': 'Bearer {}'.format(casting_assitant_token)}
-    #     res = self.client().post('/actors', headers=headers, json=self.new_actor)
-    #     data = json.loads(res.data)
-    #     self.assertEqual(res.status_code, 403)
-    #     self.assertEqual(data, {
-    #         "code": "unauthorized",
-    #         "description": "Permission not found."
-    #     })
-    # def test_get_all_categories(self):
-    #     res = self.client().get('/categories')
-    #     data = json.loads(res.data)
-
-    #     self.assertEqual(res.status_code, 200)
-    #     self.assertEqual(data['success'], True)
-    #     self.assertTrue(data['total_categories'])
-    #     self.assertTrue(len(data['categories']))
-
-    # def test_404_error_get_all_categories(self):
-    #     res = self.client().get('/categories/wqrwtwe')
-    #     data = json.loads(res.data)
-
-    #     self.assertEqual(res.status_code, 404)
-    #     self.assertEqual(data['success'], False)
-    #     self.assertEqual(data['message'], "Not found")
-
-    # def test_delete_questions(self):
-    #     res = self.client().delete('/questions/11')
-    #     data = json.loads(res.data)
-
-    #     if res.status_code == 200:
-    #         self.assertEqual(res.status_code, 200)
-    #         self.assertEqual(data['success'], True)
-    #     else:
-    #         self.assertEqual(res.status_code, 422)
-    #         self.assertEqual(data['success'], False)
-
-    # def test_422_error_delete_questions(self):
-    #     res = self.client().delete('/questions/99999')
-    #     data = json.loads(res.data)
-
-    #     self.assertEqual(res.status_code, 422)
-    #     self.assertEqual(data['success'], False)
-
-    # def test_404_error_delete_questions(self):
-    #     res = self.client().delete('/questions/qeeqe')
-    #     data = json.loads(res.data)
-
-    #     self.assertEqual(res.status_code, 404)
-    #     self.assertEqual(data['success'], False)
-
-    # def test_create_questions(self):
-    #     res = self.client().post('/questions', json=self.new_question)
-    #     data = json.loads(res.data)
-
-    #     self.assertEqual(res.status_code, 200)
-    #     self.assertEqual(data['success'], True)
-    #     self.assertTrue(data['total questions'])
-    #     self.assertTrue(len(data['questions']))
-
-    # def test_405_error_create_questions(self):
-    #     res = self.client().post('/questions/10', json=self.new_question)
-    #     data = json.loads(res.data)
-
-    #     self.assertEqual(res.status_code, 405)
-    #     self.assertEqual(data['success'], False)
-
-    # def test_search_question(self):
-    #     res = self.client().post('/search', json=self.search_data)
-    #     data = json.loads(res.data)
-
-    #     self.assertEqual(res.status_code, 200)
-    #     self.assertEqual(data['success'], True)
-
-    # def test_404_error_search_question(self):
-    #     res = self.client().post('/search/233', json=self.search_data)
-    #     data = json.loads(res.data)
-
-    #     self.assertEqual(res.status_code, 404)
-    #     self.assertEqual(data['success'], False)
-
-    # def test_quizzes(self):
-    #     res = self.client().post('/quizzes', json=self.quizzes_data)
-    #     data = json.loads(res.data)
-
-    #     self.assertEqual(res.status_code, 200)
-    #     self.assertEqual(data['success'], True)
-
-    # def test_422_error_quizzes(self):
-    #     res = self.client().post('/quizzes', json=self.search_data)
-    #     data = json.loads(res.data)
-
-    #     self.assertEqual(res.status_code, 422)
-    #     self.assertEqual(data['success'], False)
-
-    # def test_404_error_quizzes(self):
-    #     res = self.client().post('/quizzes/1', json=self.search_data)
-    #     data = json.loads(res.data)
-
-    #     self.assertEqual(res.status_code, 404)
-    #     self.assertEqual(data['success'], False)
 
 
 # Make the tests conveniently executable
